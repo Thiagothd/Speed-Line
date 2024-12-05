3847c0c02ed74a27c260da55b0ddf3d9eb728915
Thiagothd <thiagoantunes.oliver20@gmail.com>
2024-12-05 02:47:33 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
-export default {
-  base: '/Speed-Line/',
-};
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/Speed-Line/', // Adicione o nome do repositório aqui
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
